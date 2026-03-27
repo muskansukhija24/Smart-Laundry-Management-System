7d3d97d50653cca283966eaad4d4516e7230b351
Muskan Sukhija <sukhijamuskan23@gmail.com>
2026-03-27 22:52:49 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    static int orderCounter = 1;
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         LaundryService service = new LaundryService();
+        ArrayList<Order> orders = new ArrayList<>();
 
         System.out.println("=== Smart Laundry Management System ===");
 
         while (true) {
             System.out.println("\n1. Place Order");
-            System.out.println("2. Exit");
+            System.out.println("2. View Orders");
+            System.out.println("3. Exit");
             System.out.print("Choose option: ");
 
             int choice = sc.nextInt();
 
             if (choice == 1) {
-                sc.nextLine(); // clear buffer
+                sc.nextLine();
 
                 System.out.print("Enter your name: ");
                 String name = sc.nextLine();
@@ -29,10 +34,21 @@ public class Main {
 
                 double price = service.calculatePrice(serviceType, qty);
 
-                System.out.println("Total Price: " + price);
+                Order order = new Order(orderCounter++, name, serviceType, qty, price);
+                orders.add(order);
+
                 System.out.println("Order placed successfully!");
 
             } else if (choice == 2) {
+                if (orders.isEmpty()) {
+                    System.out.println("No orders found!");
+                } else {
+                    for (Order o : orders) {
+                        o.displayOrder();
+                    }
+                }
+
+            } else if (choice == 3) {
                 System.out.println("Thank you!");
                 break;
             } else {
