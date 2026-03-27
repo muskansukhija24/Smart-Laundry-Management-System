1ac19f221e7fc11c9ee519386415e25afb8cbc8d
Muskan Sukhija <sukhijamuskan23@gmail.com>
2026-03-27 22:51:07 +0530

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -1,8 +1,26 @@
-class Order {
+public class Order {
     int orderId;
     String userName;
     String serviceType;
     int quantity;
     double price;
     String status;
+
+    public Order(int orderId, String userName, String serviceType, int quantity, double price) {
+        this.orderId = orderId;
+        this.userName = userName;
+        this.serviceType = serviceType;
+        this.quantity = quantity;
+        this.price = price;
+        this.status = "Pending";
+    }
+
+    public void displayOrder() {
+        System.out.println("\nOrder ID: " + orderId);
+        System.out.println("User: " + userName);
+        System.out.println("Service: " + serviceType);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Price: " + price);
+        System.out.println("Status: " + status);
+    }
 }
