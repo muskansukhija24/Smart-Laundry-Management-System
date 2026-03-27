1cfe3ccdcfc0c7d644c0dfde77270993eb39a1ad
Muskan Sukhija <sukhijamuskan23@gmail.com>
2026-03-27 22:38:05 +0530

    Update LaundryService.java

--- src/LaundryService.java
+++ src/LaundryService.java
@@ -4,3 +4,20 @@ class LaundryService {
         return 0;
     }
 }
+
+public class LaundryService {
+
+    public double calculatePrice(String serviceType, int quantity) {
+        double rate = 0;
+
+        if (serviceType.equalsIgnoreCase("wash")) {
+            rate = 10;
+        } else if (serviceType.equalsIgnoreCase("iron")) {
+            rate = 5;
+        } else {
+            System.out.println("Invalid service!");
+        }
+
+        return rate * quantity;
+    }
+}
