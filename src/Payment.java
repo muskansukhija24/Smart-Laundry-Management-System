867cdd43dcd0e87d4cab3de29d20ccbeb4387a27
Muskan Sukhija <sukhijamuskan23@gmail.com>
2026-03-27 22:51:58 +0530

    Update Payment.java

--- src/Payment.java
+++ src/Payment.java
@@ -1,5 +1,11 @@
-class Payment {
+public class Payment {
     int paymentId;
     double amount;
     String status;
+
+    public Payment(int paymentId, double amount) {
+        this.paymentId = paymentId;
+        this.amount = amount;
+        this.status = "Paid";
+    }
 }
