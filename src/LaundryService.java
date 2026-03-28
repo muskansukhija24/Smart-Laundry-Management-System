009bee265dc20e1fdca573922ee8c7c11f3b50cf
Muskan Sukhija <sukhijamuskan23@gmail.com>
2026-03-28 19:18:32 +0530

    Update LaundryService.java

--- src/LaundryService.java
+++ src/LaundryService.java
@@ -3,25 +3,22 @@ public double calculatePrice(String serviceType, String clothType, int quantity)
     double rate = 0;
 
     if (serviceType.equalsIgnoreCase("wash")) {
-        if (clothType.equalsIgnoreCase("woolen")) {
-            rate = 15;
-        } else {
-            rate = 10;
-        }
+
+        if (clothType.equalsIgnoreCase("woolen")) rate = 15;
+        else rate = 10;
 
     } else if (serviceType.equalsIgnoreCase("iron")) {
-        if (clothType.equalsIgnoreCase("woolen")) {
-            rate = 8;
-        } else {
-            rate = 5;
-        }
+
+        if (clothType.equalsIgnoreCase("woolen")) rate = 8;
+        else rate = 5;
 
     } else if (serviceType.equalsIgnoreCase("wash+iron")) {
-        if (clothType.equalsIgnoreCase("woolen")) {
-            rate = 20;
-        } else {
-            rate = 15;
-        }
+
+        if (clothType.equalsIgnoreCase("woolen")) rate = 20;
+        else rate = 15;
+
+    } else {
+        System.out.println("Invalid service!");
     }
 
     return rate * quantity;
